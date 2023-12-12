6b8a1ae9e42a7da2a6d35e1337c05844b1e1ae4e
Arseniy <144226583+Arseniygg1@users.noreply.github.com>
2023-12-12 15:13:47 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,28 @@
+﻿Program zad3;
+type mass = array [1..10] of integer;
+var
+  A:mass;
+  i:integer;
+function fist_pol(A:mass):integer;
+var j:integer;
+begin 
+  for j:=1 to 10 do 
+  begin
+    if A[j] > 0 then
+      begin
+      fist_pol:= j;
+      break
+      end
+    else 
+      fist_pol:= 0;
+  end;
+end;
+begin 
+  for i:=1 to 10 do
+    begin
+    A[i]:= random(-10,10);
+    write(A[i]:3);
+    end;
+  writeln();
+  writeln(fist_pol(A))
+end.
---
+++ 9.pas
@@ -0,0 +1,55 @@
+﻿program zad9;
+type dwomass = array [1..8,1..8] of integer;mass = array [1..8] of integer;
+var 
+  x,y,min,sredar,sum,k:integer;
+  A:dwomass;
+  b:mass;
+begin
+min:=5;
+k:=0;
+sum:=0;
+  for x:=1 to 8 do
+  begin
+    writeln();
+    for y:=1 to 8 do 
+    begin
+      A[x,y]:=random(-1,5);
+      write(A[x,y]:3)
+    end;
+  end;
+  
+  
+  for x:=1 to 8 do
+  begin
+    for y:=1 to 8 do
+    begin
+      if A[x,y] < min then
+      begin
+        min:=A[x,y];
+      end;
+    end;
+  B[x]:=min;
+  min:=5;
+  end;
+  writeln();
+  for x:=1 to 8 do
+  begin
+    write(B[x]:4)
+  end;
+  for x:=1 to 8 do
+  begin
+    sum += B[x];
+    k += 1;
+  end;
+  writeln();
+  sredar:=round(sum/k);
+  for x:=1 to 8 do
+  begin
+    if B[x] = sredar then 
+    begin
+      writeln('есть');
+      break
+    end  
+  end;
+end.
+
